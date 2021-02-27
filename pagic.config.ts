1a3fe0daf6e86b4accaac5f94db69d6eba416236
Dean Xu <373216024@qq.com>
2021-02-27 21:01:13 +0800

    init pagic.config.ts

---
+++ pagic.config.ts
@@ -0,0 +1,44 @@
+import {PagicConfig} from 'https://deno.land/x/pagic@v1.2.0/src/Pagic.ts'
+
+export default {
+  srcDir: 'src',
+  theme: 'blog',
+
+  title: 'XDean的博客',
+
+  plugins: ['blog'],
+  blog: {
+    root: '/posts',
+    social: {
+      github: 'xdean/blog-pagic',
+      email: 'dean.xu@asml.com',
+    },
+  },
+  nav: [
+    {
+      text: '首页',
+      link: '/index.html',
+      icon: 'czs-home-l',
+    },
+    {
+      text: '分类',
+      link: '/categories/index.html',
+      icon: 'czs-category-l',
+    },
+    {
+      text: '标签',
+      link: '/tags/index.html',
+      icon: 'czs-tag-l',
+    },
+    {
+      text: '关于',
+      link: '/about/index.html',
+      icon: 'czs-about-l',
+    },
+    {
+      text: '归档',
+      link: '/archives/index.html',
+      icon: 'czs-box-l',
+    },
+  ]
+} as PagicConfig;
