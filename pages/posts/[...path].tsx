29f1fef4860c1a928ae0dfb4363abb3c414f0cb1
Dean Xu <373216024@qq.com>
2021-03-03 23:12:05 +0800

    render post

--- pages/posts/[...path].tsx
+++ pages/posts/[...path].tsx
@@ -1,31 +1,60 @@
 import {GetStaticPaths, GetStaticProps} from 'next'
-import {promises as fs} from "fs";
 import path from "path";
 import {walkFiles} from "../../util/util";
+import {promises as fs} from "fs";
+import {useState} from "react";
+import MarkdownIt from 'markdown-it'
+import MarkdownItAnchor from 'markdown-it-anchor'
 
-const Post = (props) => {
-  console.log('hah', props)
+type Props = {
+  content: string
+}
+
+type Params = {
+  path: string[]
+}
+
+const Post = (props: Props) => {
   return (
-    <div>
-      {props.toString()}
+    <div dangerouslySetInnerHTML={{__html: props.content}}>
     </div>
   )
 }
 
-export const getStaticProps: GetStaticProps = async ctx => {
+export const getStaticProps: GetStaticProps<Props, Params> = async ctx => {
+  const postFile = path.join(process.cwd(), 'posts', ...ctx.params.path)
+  const content = await fs.readFile(postFile, 'utf-8')
+  const md = MarkdownIt()
+    .use(MarkdownItAnchor, {
+      level: [2],
+      slugify: (hash: string) =>
+        encodeURIComponent(
+          hash
+            .trim()
+            .toLowerCase()
+            .replace(/ /g, '-')
+            .replace(/\W/g, '')
+        ),
+      permalink: true,
+      permalinkSpace: true,
+      permalinkSymbol: '§',
+      permalinkBefore: true,
+    })
+  const res = md.render(content)
   return {
-    props: {},
+    props: {
+      content: res
+    },
   }
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
   const postsDir = path.join(process.cwd(), 'posts');
   const filenames = await walkFiles(postsDir, {ext: ['md']})
   const paths = []
   for await (const f of filenames) {
     paths.push({params: {path: f}})
   }
-  console.log(paths)
   return {
     paths: paths,
     fallback: false,
