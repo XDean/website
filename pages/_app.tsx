dcc90e2cb6cac1c2b8454550ab2bccb073758728
Dean Xu <373216024@qq.com>
2021-03-06 11:30:20 +0800

    Header.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
-import {CssBaseline} from "@material-ui/core";
+import {CssBaseline, Divider} from "@material-ui/core";
+import {HeaderView} from "../src/components/Header";
+import Head from "next/head";
 
 function MyApp({Component, pageProps}) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+        <title>XDean</title>
+      </Head>
       <CssBaseline/>
+      <HeaderView/>
+      <Divider/>
       <Component {...pageProps} />
     </>
   )
