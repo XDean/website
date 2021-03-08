ab11bad428fc628112de7042185cbc851770ff03
dxu <dean.xu@asml.com>
2021-03-08 15:06:44 +0800

    use markdown-toc

--- src/components/blog/Post.tsx
+++ src/components/blog/Post.tsx
@@ -3,7 +3,6 @@ import MarkdownItTitle from "markdown-it-title";
 import MarkdownItToc from "markdown-it-toc-done-right";
 import {PostMeta} from "../../posts/domain";
 import React, {ReactElement, useMemo} from "react";
-import Link from 'next/link'
 import 'github-markdown-css/github-markdown.css'
 import {Button, createStyles, Divider, makeStyles, Tooltip, Typography} from "@material-ui/core";
 import 'highlight.js/styles/stackoverflow-light.css'
@@ -15,10 +14,11 @@ import RemarkMathPlugin from 'remark-math';
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 import {idea as syntaxStyle} from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import {MyLink} from "../util/Link";
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import {PostTag} from "./PostTag";
 import {format} from "date-fns";
 import {GithubComment} from "../util/GithubComment";
+import MarkdownToc from 'markdown-toc-unlazy'
+import uslug from 'uslug'
 
 const useStyles = makeStyles(theme => createStyles({
   'toc': {
@@ -54,28 +54,38 @@ export type PostProps = {
 
 export const PostView = (props: PostProps) => {
   const classes = useStyles()
-  const {title, toc} = useMemo(() => renderMarkdown(props.content, props.meta), [props.content, classes])
-  const hasToc = props.meta.toc !== false
+  const {title} = useMemo(() => renderMarkdown(props.content, props.meta), [props.content, classes])
+  const tocData = extractMarkdownToc(props.content)
+  const hasToc = props.meta.toc !== false && tocData.content.length > 0
   const hasUpdate = props.meta.updated && props.meta.updated !== props.meta.created
+
   return (
     <div style={{maxWidth: hasToc ? 1200 : 1000, minWidth: '50%', display: 'flex', width: '100vw', margin: '0 20px'}}>
-      {hasToc && (
-        <>
-          <div style={{position: 'relative'}} className={classes.tocContainer}>
-            <div style={{position: 'fixed', maxWidth: 200, zIndex: 10, marginTop: 100}}>
-              <Typography variant={'h5'}>
-                <a href={'#title'}>
-                  目录
-                </a>
-              </Typography>
-              <div dangerouslySetInnerHTML={{__html: toc}} className={classes.toc}/>
-            </div>
-            <div dangerouslySetInnerHTML={{__html: toc}} style={{opacity: 0, maxWidth: 200}}
-                 className={classes.toc}/>
+      {hasToc && function () {
+        const toc = (
+          <div style={{minWidth: 80, maxWidth: 200}}>
+            <Typography variant={'h5'}>
+              <a href={'#title'}>
+                目录
+              </a>
+            </Typography>
+            <ReactMarkdown source={tocData.content} className={classes.toc}/>
           </div>
-          <Divider orientation={"vertical"} style={{marginLeft: 20, marginRight: 20}}/>
-        </>
-      )}
+        )
+        return (
+          <>
+            <div style={{position: 'relative'}} className={classes.tocContainer}>
+              <div style={{position: 'fixed', zIndex: 10, marginTop: 100}}>
+                {toc}
+              </div>
+              <div style={{opacity: 0}}>
+                {toc}
+              </div>
+            </div>
+            <Divider orientation={"vertical"} style={{marginLeft: 20, marginRight: 20}}/>
+          </>
+        )
+      }()}
       <div style={{width: 0, flexGrow: 1}}>
         <Typography variant={"h4"} id={'title'} className={classes.title}>
           {title}
@@ -98,7 +108,7 @@ export const PostView = (props: PostProps) => {
                              if (props.level === 1) {
                                return null
                              }
-                             const slug = slugify(innerText(<>{props.children}</>))
+                             const slug = uslug(innerText(<>{props.children}</>))
                              return React.createElement(`h${props.level}`, {
                                id: slug,
                                style: {
@@ -119,28 +129,28 @@ export const PostView = (props: PostProps) => {
         </MathJax.Provider>
         {(props.prev || props.next) &&
         <div style={{display: 'flex', marginTop: 20}}>
-            <div style={{width: 0, flexGrow: 1}}>
-              {props.prev && (
-                <MyLink href={props.prev.link}>
-                  <Button variant={"outlined"}>
-                    <Typography variant={"h6"}>
-                      上一篇: {props.prev.title}
-                    </Typography>
-                  </Button>
-                </MyLink>
-              )}
-            </div>
-            <div style={{width: 0, flexGrow: 1, textAlign: 'right'}}>
-              {props.next && (
-                <MyLink href={props.next.link}>
-                  <Button variant={"outlined"}>
-                    <Typography variant={"h6"}>
-                      下一篇: {props.next.title}
-                    </Typography>
-                  </Button>
-                </MyLink>
-              )}
-            </div>
+          <div style={{width: 0, flexGrow: 1}}>
+            {props.prev && (
+              <MyLink href={props.prev.link}>
+                <Button variant={"outlined"}>
+                  <Typography variant={"h6"}>
+                    上一篇: {props.prev.title}
+                  </Typography>
+                </Button>
+              </MyLink>
+            )}
+          </div>
+          <div style={{width: 0, flexGrow: 1, textAlign: 'right'}}>
+            {props.next && (
+              <MyLink href={props.next.link}>
+                <Button variant={"outlined"}>
+                  <Typography variant={"h6"}>
+                    下一篇: {props.next.title}
+                  </Typography>
+                </Button>
+              </MyLink>
+            )}
+          </div>
         </div>
         }
         <div style={{marginTop: 30}}>
@@ -152,15 +162,12 @@ export const PostView = (props: PostProps) => {
   )
 }
 
-function slugify(hash: string) {
-  return encodeURIComponent(
-    hash
-      .trim()
-      .toLowerCase()
-      .replace(/ /g, '-')
-      .replace(/[ `~!@#$%^&*()=+\[{\]}\\|;:'",<.>/?·～！¥…（）—【「】」、；：‘“’”，《。》？]/g, '')
-      .replace(/[\uff00-\uffff]/g, '')
-  );
+function extractMarkdownToc(content: string) {
+  return MarkdownToc(content, {
+    slugify: uslug,
+    maxDepth: 2,
+    firsth1: false,
+  })
 }
 
 function renderMarkdown(content: string, meta: PostMeta) {
@@ -174,7 +181,7 @@ function renderMarkdown(content: string, meta: PostMeta) {
     mdRenderer.use(MarkdownItToc, {
       containerClass: 'toc',
       level: [2, 3],
-      slugify,
+      uslug,
       callback: (html: string) => {
         env.toc = html;
       },
