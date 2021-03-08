92d7b6f8d9ab5650b1124dbdcc563be8e705988c
dxu <dean.xu@asml.com>
2021-03-08 15:26:10 +0800

    update meta

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,28 @@
-import {Button, Toolbar, Typography} from "@material-ui/core";
+import {Toolbar, Typography} from "@material-ui/core";
 import {MyLink} from "./util/Link";
 
 export const HeaderView = () => {
   return (
     <Toolbar>
-      <MyLink href={'/'}>
-        <Button variant={"text"} style={{marginLeft: 0}}>
-          <Typography variant={'h3'}>
-            XDean
-          </Typography>
-        </Button>
+      <MyLink href={'/'} style={{marginLeft: 0}}>
+        <Typography variant={'h3'}>
+          XDean
+        </Typography>
       </MyLink>
-      <MyLink href={'/blog'}>
-        <Button variant={"text"} style={{marginLeft: 30}}>
-          <Typography variant={'h5'}>
-            Blog
-          </Typography>
-        </Button>
+      <MyLink href={'/blog'} style={{marginLeft: 30}}>
+        <Typography variant={'h5'}>
+          Blog
+        </Typography>
       </MyLink>
-      <MyLink href={'/tools'}>
-        <Button variant={"text"} style={{marginLeft: 20}}>
-          <Typography variant={'h5'}>
-            工具箱
-          </Typography>
-        </Button>
+      <MyLink href={'/tools'} style={{marginLeft: 20}}>
+        <Typography variant={'h5'}>
+          工具箱
+        </Typography>
       </MyLink>
-      <MyLink href={'/about'}>
-        <Button variant={"text"} style={{marginLeft: 20}}>
-          <Typography variant={'h5'}>
-            关于我
-          </Typography>
-        </Button>
+      <MyLink href={'/about'} style={{marginLeft: 20}}>
+        <Typography variant={'h5'}>
+          关于我
+        </Typography>
       </MyLink>
     </Toolbar>
   )
