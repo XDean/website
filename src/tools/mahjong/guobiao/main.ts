748f04c19a69e49c7601946c3d011e16db88bc2f
Dean Xu <373216024@qq.com>
2021-03-08 19:00:55 +0800

    [guobiao] find ke shun combination

--- src/tools/mahjong/guobiao/main.ts
+++ src/tools/mahjong/guobiao/main.ts
@@ -1,3 +1,92 @@
-export function calculate(){
+import {Combination, Dui, Hand, Hu, Ke, Shun, Tile, Tiles} from "./type";
 
+export function calculate(hand: Hand): Hu {
+  if (hand.count != 14) {
+    throw 'hand count must be 14'
+  }
+
+
+  return new Hu(new Combination([]), [])
+}
+
+function* findAllCombinations(tiles: Tiles): Generator<Combination> {
+  const duis = findDui(tiles);
+  if (duis.length === 0) {
+    if (tiles.length < 14) {
+      return
+    } else {
+      // bukao, yao13
+    }
+  } else {
+    for (let [leftTiles, dui] of duis) {
+      const combs = findShunKeCombinations(leftTiles)
+      for (let comb of combs) {
+        yield comb.with(dui)
+      }
+    }
+  }
+}
+
+function* findShunKeCombinations(tiles: Tiles): Generator<Combination> {
+  if (tiles.length === 0) {
+    return
+  }
+  const first = tiles[0]
+  for (let [left, ke] of findKe(tiles, first)) {
+    for (let sub of findShunKeCombinations(left)) {
+      yield sub.with(ke)
+    }
+  }
+  for (let [left, shun] of findShun(tiles, first)) {
+    for (let sub of findShunKeCombinations(left)) {
+      yield sub.with(shun)
+    }
+  }
+}
+
+function findDui(tiles: Tiles): [Tiles, Dui][] {
+  const exist = []
+  const dups = []
+  for (let tile of tiles.tiles) {
+    if (exist.indexOf(tile) === -1) {
+      exist.push(tile)
+    } else if (dups.indexOf(tile) === -1) {
+      dups.push(tile)
+    }
+  }
+  const results = []
+  for (let dup of dups) {
+    const [left] = tiles.split(dup, dup);
+    results.push([left, new Dui(dup)])
+  }
+  return results
+}
+
+
+function* findShun(tiles: Tiles, tile: Tile): Generator<[Tiles, Shun]> {
+  if (tile.type === 'z') {
+    return
+  }
+  const subTiles = tiles.filterType(tile.type).filterShunPoint(tile.point);
+  if (subTiles.length < 2) {
+    return
+  }
+  for (let [a, b] of subTiles.pairs()) {
+    const diff = Math.abs(a.point - b.point)
+    if (diff === 1 || diff === 2) {
+      const [left] = tiles.split(a, b, tile)
+
+      yield [left, new Shun(new Tiles([a, b, tile]).minPointTile)]
+    }
+  }
+}
+
+function findKe(tiles: Tiles, tile: Tile): [Tiles, Ke][] {
+  const sames = tiles.filterType(tile.type).filterPoint(tile.point)
+  if (sames.length > 1) {
+    const [left] = tiles.split(tile, tile, tile);
+    return [[left, new Ke(tile)]]
+  } else {
+    return []
+  }
 }
--- src/tools/mahjong/guobiao/type.ts
+++ src/tools/mahjong/guobiao/type.ts
@@ -1,11 +1,108 @@
 import assert from "assert";
 
+
+export type Options = {
+  zimo: boolean
+}
+
 export type TileType = 'p' | 's' | 'm' | 'z'
 export type TilePoint = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 
 export type Tile = {
   type: TileType
   point: TilePoint
+}
+
+export class Tiles {
+  constructor(
+    readonly tiles: Tile[],
+  ) {
+  }
+
+  get length() {
+    return this.tiles.length
+  }
+
+  split(...removes: Tile[]): [Tiles, Tiles] {
+    const copy = [...this.tiles]
+    const removed = []
+    for (let remove of removes) {
+      const index = copy.indexOf(remove);
+      if (index != -1) {
+        copy.splice(index)
+        removed.push(remove)
+      }
+    }
+    return [new Tiles(copy), new Tiles(removed)]
+  }
+
+  filterType(type: TileType) {
+    return new Tiles(this.tiles.filter(t => t.type === type))
+  }
+
+  filterPoint(point: number) {
+    return new Tiles(this.tiles.filter(t => t.point === point))
+  }
+
+  filterShunPoint(point: number) {
+    return new Tiles(this.tiles.filter(t => t.point !== point && Math.abs(t.point - point) < 3))
+  }
+
+  * pairs() {
+    for (let i = 0; i < this.tiles.length; i++) {
+      for (let j = i + 1; j < this.tiles.length; j++) {
+        yield [this.tiles[i], this.tiles[j]]
+      }
+    }
+  }
+
+  get minPointTile(): Tile {
+    let min = this.tiles[0]
+    for (let tile of this.tiles) {
+      if (tile.point < min.point) {
+        min = tile
+      }
+    }
+    return min
+  }
+}
+
+export class Hand {
+  constructor(
+    readonly tiles: Tiles, // last one is last card
+    readonly mings: Ming[] = [],
+    readonly options: Options,
+  ) {
+  }
+
+  get count() {
+    return this.tiles.length + 3 * this.mings.length
+  }
+
+  get lastTile() {
+    return this.tiles[this.tiles.length - 1]
+  }
+}
+
+export class Combination {
+  constructor(
+    readonly mians: Mian[]
+  ) {
+  }
+
+  with = (m: Mian) => new Combination([...this.mians, m])
+}
+
+export interface Fan {
+  score: number
+}
+
+export class Hu {
+  constructor(
+    readonly combination: Combination,
+    readonly fans: Fan[],
+  ) {
+  }
 }
 
 export class Chi {
