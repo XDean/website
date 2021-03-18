c3603a0ae26a3b5d6ce64246a5acc7ddb134f604
Dean Xu <373216024@qq.com>
2021-03-18 20:44:16 +0800

    try gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,22 @@ import {HeaderView} from "../src/components/Header";
 import Head from "next/head";
 import {FooterView} from "../src/components/Footer";
 import "tailwindcss/tailwind.css";
+import {useEffect} from 'react'
+import {useRouter} from 'next/router'
+import * as gtag from '../src/util/gtag'
 
 function MyApp({Component, pageProps}) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
