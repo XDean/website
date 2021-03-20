4e0879be3c994ab423fa0eaa03f9541c60bff554
Dean Xu <373216024@qq.com>
2021-03-20 08:33:34 +0800

    fix layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,17 +9,17 @@ export const HeaderView = () => {
       <div className={'inline w-8 h-8 md:w-12 md:h-12 mr-2 md:mr-8 bg-contain bg-no-repeat'}
            style={{backgroundImage: logoHover ? `url('/xd.png')` : `url('/favicon.ico')`}}
            onMouseEnter={() => setLogoHover(true)} onMouseLeave={() => setLogoHover(false)}/>
-      <div className={'flex flex-row flex-grow items-center'}>
-        <MyLink href={'/'} className={'text-2xl md:text-3xl mr-3 md:mr-8'}>
+      <div className={'flex flex-row flex-grow items-center text-xl md:text-3xl'}>
+        <MyLink href={'/'} className={'mr-3 md:mr-8'}>
           主页
         </MyLink>
-        <MyLink href={'/blog'} className={'text-2xl md:text-3xl mr-3 md:mr-8'}>
+        <MyLink href={'/blog'} className={'mr-3 md:mr-8'}>
           博客
         </MyLink>
-        <MyLink href={'/tools'} className={'text-2xl md:text-3xl mr-3 md:mr-8'}>
+        <MyLink href={'/tools'} className={'mr-3 md:mr-8'}>
           工具箱
         </MyLink>
-        <MyLink href={'/about'} className={'text-2xl md:text-3xl mr-3 md:mr-8'}>
+        <MyLink href={'/about'} className={'mr-3 md:mr-8'}>
           关于我
         </MyLink>
       </div>
