de695ca3c8ae55c4b1774c372a2da60aba7495a7
Dean Xu <373216024@qq.com>
2021-03-23 18:56:01 +0800

    add sitemap

---
+++ sitemap.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://xdean.cn',
+    generateRobotsTxt: true,
+    exclude:[
+        '/blog/page/*',
+        '/blog/archives/*',
+    ]
+}
