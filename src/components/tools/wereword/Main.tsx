4e949b914a0355fa8c4bb8653332e707bf9ed556
Dean Xu <373216024@qq.com>
2021-03-25 21:21:33 +0800

    [wereword] start game page

--- src/components/tools/wereword/Main.tsx
+++ src/components/tools/wereword/Main.tsx
@@ -6,97 +6,111 @@ import {Loading} from "../../util/Loading";
 export const WerewordMain = () => {
   const [state, send] = useMachine(() => createWerewordMachine({}))
   return (
-    <div className={'flex items-center justify-evenly w-full'}>
-      <div className={'grid grid-cols-1 gap-20'}>
-        <div>
-          {state.context.leftSeconds > 0 && (
-            <button className={'btn-blue'} onClick={() => send('TICK_ALL')}>快进</button>
-          )}
-          <button className={'btn-blue'} onClick={() => send('STOP')}>停止</button>
+    <div className={'w-11/12 max-w-screen-md'}>
+      <div className={'h-32 w-32 text-center m-auto bg-contain bg-no-repeat'}
+           style={{backgroundImage: 'url(/tools/wereword/logo.webp)'}}/>
+      <div className={'text-5xl mb-4 text-center'}>狼人真言</div>
+      <div className={'text-2xl mb-4 text-center'}>请先购买正版《狼人真言》<br/>然后搭配使用本工具进行游戏</div>
+      {state.matches('prepare') && (
+        <div className={'flex flex-col items-center text-4xl'}>
+          <div>正在加载</div>
+          <Loading className={'h-12 w-12 text-blue-500 mt-2'}/>
         </div>
-        {state.matches('prepare') && <Loading/>}
-        {state.matches('waiting') && (
+      )}
+      {state.matches('waiting') && (
+        <div>
+          <button className={'btn-blue text-3xl text-center block m-auto'}
+                  onClick={() => send('START')}>
+            开始游戏
+          </button>
+          <div className={'border my-4 p-2'}>
+            <div className={'text-2xl mb-1'}>选择词库</div>
+            <div>
+              {state.context.allWordSet.map(s => (
+                <div key={s.id} className={'inline-flex items-center mx-2 my-1'}>
+                  <input type="checkbox" checked={state.context.wordSet.indexOf(s) !== -1}
+                         onChange={e => send(({type: 'SELECT_WORD_SET', value: s, check: e.target.checked}))}
+                         className="h-6 w-6"/>
+                  <span className={'ml-1'}>{s.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+      {state.matches('night.start') && (
+        <div>
+          夜晚开始，全体闭眼
+        </div>
+      )}
+      {state.matches('night.cunzhang.select') && (
+        <div>
           <div>
-            {state.context.allWordSet.map(s => (
-              <div key={s.id}>
-                <input type="checkbox" checked={state.context.wordSet.indexOf(s) !== -1}
-                       onChange={e => send(({type: 'SELECT_WORD_SET', value: s, check: e.target.checked}))}
-                       className="h-6 w-6"/>
-                <span>{s.name}</span>
-              </div>
-            ))}
-            <button className={'btn-blue'} onClick={() => send('START')}>开始游戏
+            村长阶段
+          </div>
+          {state.context.words.map((w, i) => (
+            <button key={i} className={'block btn-blue'} onClick={() => send({type: 'SELECT_WORD', value: w})}>
+              {w}
             </button>
-          </div>
-        )}
-        {state.matches('night.start') && (
+          ))}
+          <button className={'btn-blue'} onClick={() => send('CHANGE_WORD')}>换一组词</button>
+        </div>
+      )}
+      {state.matches('night.cunzhang.confirm') && (
+        <div>
+          确认魔法咒语 ({state.context.leftSeconds}秒)
           <div>
-            夜晚开始，全体闭眼
+            {state.context.answer}
           </div>
-        )}
-        {state.matches('night.cunzhang.select') && (
+        </div>
+      )}
+      {state.matches('night.xianzhi') && (
+        <div>
+          先知阶段 ({state.context.leftSeconds}秒)
           <div>
-            <div>
-              村长阶段
-            </div>
-            {state.context.words.map((w, i) => (
-              <button key={i} className={'block btn-blue'} onClick={() => send({type: 'SELECT_WORD', value: w})}>
-                {w}
-              </button>
-            ))}
-            <button className={'btn-blue'} onClick={() => send('CHANGE_WORD')}>换一组词</button>
+            {state.context.answer}
           </div>
-        )}
-        {state.matches('night.cunzhang.confirm') && (
+        </div>
+      )}
+      {state.matches('night.langren') && (
+        <div>
+          狼人阶段 ({state.context.leftSeconds}秒)
           <div>
-            确认魔法咒语 ({state.context.leftSeconds}秒)
-            <div>
-              {state.context.answer}
-            </div>
+            {state.context.answer}
           </div>
-        )}
-        {state.matches('night.xianzhi') && (
-          <div>
-            先知阶段 ({state.context.leftSeconds}秒)
-            <div>
-              {state.context.answer}
-            </div>
-          </div>
-        )}
-        {state.matches('night.langren') && (
-          <div>
-            狼人阶段 ({state.context.leftSeconds}秒)
-            <div>
-              {state.context.answer}
-            </div>
-          </div>
-        )}
-        {state.matches('night.end') && (
-          <div>
-            夜晚结束，全体睁眼
-          </div>
-        )}
-        {state.matches('daytime') && (
+        </div>
+      )}
+      {state.matches('night.end') && (
+        <div>
+          夜晚结束，全体睁眼
+        </div>
+      )}
+      {state.matches('daytime') && (
+        <div>
+          剩余({state.context.leftSeconds}秒)
+          <button className={'btn-blue'}
+                  onClick={() => send('PAUSE')}>{state.matches('daytime.running') ? '暂停' : '继续'}</button>
+          <button className={'btn-blue'}
+                  onClick={() => send('RIGHT')}>猜对了
+          </button>
+          <button className={'btn-blue'}
+                  onClick={() => send('WRONG')}>Token用完了
+          </button>
+        </div>
+      )}
+      {state.matches('find') && (
+        <div>
+          找出{state.context.findAnswer ? '先知' : '狼人'}
           <div>
             剩余({state.context.leftSeconds}秒)
-            <button className={'btn-blue'}
-                    onClick={() => send('PAUSE')}>{state.matches('daytime.running') ? '暂停' : '继续'}</button>
-            <button className={'btn-blue'}
-                    onClick={() => send('RIGHT')}>猜对了
-            </button>
-            <button className={'btn-blue'}
-                    onClick={() => send('WRONG')}>Token用完了
-            </button>
           </div>
+        </div>
+      )}
+      <div>
+        {state.context.leftSeconds > 0 && (
+          <button className={'btn-blue'} onClick={() => send('TICK_ALL')}>快进</button>
         )}
-        {state.matches('find') && (
-          <div>
-            找出{state.context.findAnswer ? '先知' : '狼人'}
-            <div>
-              剩余({state.context.leftSeconds}秒)
-            </div>
-          </div>
-        )}
+        <button className={'btn-blue'} onClick={() => send('STOP')}>停止</button>
       </div>
     </div>
   )
