5ac8ed32bdc2e9855574ff7aa0f4b57321429a19
dxu <dean.xu@asml.com>
2021-03-25 11:23:35 +0800

    wereword machine

--- src/tools/werewords/machine.ts
+++ src/tools/werewords/machine.ts
@@ -1,16 +1,50 @@
-import {actions, assign, Machine, send} from 'xstate'
+import {assign, Machine, StateSchema} from 'xstate'
 
-export type WerewordsContext = {
+
+export type WordSet = {
+  id: string
+  name: string
+}
+
+export interface WerewordsContext {
   playerCount?: number
+  wordSet?: WordSet[]
+  words?: string[]
   answer?: string
   leftSeconds?: number
+  error?: any
+}
+
+export interface WerewordsSchema extends StateSchema<WerewordsContext> {
+  states: {
+    prepare:{},
+    night:{
+      states:{
+        start:{},
+        cunzhang:{
+          states:{
+            select:{},
+            loading:{},
+            confirm:{},
+          }
+        },
+        xianzhi:{},
+        langren:{},
+        end:{}
+      },
+    },
+    daytime:{},
+    error:{},
+  }
 }
 
 export type WerewordsEvent =
-  { type: 'START', playCount: number } |
-  { type: 'END', playCount: number }
+  { type: 'START', playCount: number, wordSets: WordSet[] } |
+  { type: 'CHANGE_WORD' } |
+  { type: 'SELECT_WORD', value: string } |
+  { type: 'STOP' }
 
-export const WerewordsMachine = Machine<WerewordsContext, WerewordsEvent>({
+export const WerewordsMachine = Machine<WerewordsContext,WerewordsSchema, WerewordsEvent>({
   id: 'werewords',
   initial: 'prepare',
   context: {},
@@ -19,26 +53,65 @@ export const WerewordsMachine = Machine<WerewordsContext, WerewordsEvent>({
       on: {
         START: {
           target: 'night',
-          actions: 'start-game',
+          actions: assign((ctx, event) => ({
+            playerCount: event.playCount,
+            wordSets: event.wordSets,
+          }))
         }
       }
     },
     night: {
-      initial: 'cunzhang',
+      initial: 'start',
       states: {
-        cunzhang: {},
+        start: {},
+        cunzhang: {
+          initial: 'loading',
+          states: {
+            select: {
+              on: {
+                CHANGE_WORD: 'loading',
+                SELECT_WORD: {
+                  target: '.confirm',
+                  actions: assign({
+                    answer: (ctx, event) => event.value
+                  })
+                }
+              }
+            },
+            loading: {
+              invoke: {
+                src: (context, event) =>
+                  fetch(`/api/word/random?set=${context.wordSet!.join(',')}`)
+                    .then(res => res.json()),
+                onDone: {
+                  target: '..select',
+                  actions: assign({
+                    words: (ctx, event) => event.data
+                  })
+                },
+                onError: {
+                  target: '..error',
+                  actions: assign({
+                    error: (ctx, event) => event.data
+                  })
+                }
+              },
+              entry: () => console.log('loading word data...')
+            },
+            confirm: {},
+          },
+        },
         xianzhi: {},
-        langren: {}
+        langren: {},
+        end: {}
       }
     },
-    daytime: {}
+    daytime: {},
+    error: {},
+  },
+  on: {
+    STOP: 'prepare',
   }
 }, {
-  actions: {
-    'start-game': assign((ctx, event) => {
-      return {
-        playerCount: event.playCount
-      }
-    })
-  }
+  actions: {}
 })
