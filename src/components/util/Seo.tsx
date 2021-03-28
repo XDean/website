6f4190c31c21e03055f55adfb04c119298e05c6a
dxu <dean.xu@asml.com>
2021-03-28 16:59:44 +0800

    fix opengraph

--- src/components/util/Seo.tsx
+++ src/components/util/Seo.tsx
@@ -1,32 +1,27 @@
 import {NextSeo} from "next-seo";
 import {NextSeoProps} from "next-seo/lib/types";
 import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
 
 export const MySeo = (props: NextSeoProps) => {
   const router = useRouter()
-  const [real, setReal] = useState(props)
 
-  useEffect(() => {
-    const copy = JSON.parse(JSON.stringify(props)) as NextSeoProps
-    const url = `https://xdean.cn${router.route}`
-    const og = copy.openGraph;
-    if (!!og) {
-      if (!!og.url) {
-        og.url = new URL(og.url, url).toString()
-      } else {
-        og.url = url
-      }
-      if (!!og.images) {
-        og.images.forEach(im => {
-          im.url = new URL(im.url, url).toString()
-        })
-      }
+  const copy = JSON.parse(JSON.stringify(props)) as NextSeoProps
+  const url = `https://xdean.cn${router.route}`
+  const og = copy.openGraph;
+  if (!!og) {
+    if (!!og.url) {
+      og.url = new URL(og.url, url).toString()
+    } else {
+      og.url = url
     }
-    setReal(copy)
-  }, [props, router])
+    if (!!og.images) {
+      og.images.forEach(im => {
+        im.url = new URL(im.url, url).toString()
+      })
+    }
+  }
 
   return (
-    <NextSeo {...real}/>
+    <NextSeo {...copy}/>
   )
 }
