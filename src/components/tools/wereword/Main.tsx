630bc20ae8d1d14367f8f275278678c01183ddff
Dean Xu <373216024@qq.com>
2021-03-28 08:35:20 +0800

    [wereword] add title

--- src/components/tools/wereword/Main.tsx
+++ src/components/tools/wereword/Main.tsx
@@ -4,17 +4,22 @@ import {context} from "@opentelemetry/api";
 import {Loading} from "../../util/Loading";
 import {VscRefresh} from 'react-icons/vsc'
 import clsx from "clsx";
-import {CSSProperties} from "react";
+import React, {CSSProperties} from "react";
 import {WerewordSetup} from "./Setup";
 import {WerewordDayTime} from "./Daytime";
 import {WerewordNight} from "./Night";
 import {WerewordOver} from "./Over";
+import Head from "next/head";
+import {motion} from "framer-motion";
 
 export const WerewordMain = () => {
   const [state, send, service] = useMachine(() => createWerewordMachine({nightTime: 1000}))
 
   return (
     <div className={'w-11/12 max-w-screen-md h-full flex flex-col'}>
+      <Head>
+        <title>狼人真言网页版 - XDean</title>
+      </Head>
       {state.matches('setup') && <WerewordSetup service={service}/>}
       {state.matches('play') && (
         <>
