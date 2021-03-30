a8282c69a8cc48968cedccb2adcc3d9c1bbfe4cb
Dean Xu <373216024@qq.com>
2021-03-30 21:40:40 +0800

    fix register workbox

--- src/util/register_workbox.ts
+++ src/util/register_workbox.ts
@@ -11,18 +11,7 @@ export function register(
     start_url: string,
   }) {
   if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-    // @ts-ignore
-    (window as any).workbox = new Workbox(sw, {scope: scope})
-
-    (window as any).workbox.addEventListener('activated', function (event) {
-      if (!event.isUpdate) {
-        caches.keys().then(function (c) {
-          if (!c.includes('start-url')) {
-            fetch(start_url)
-          }
-        })
-      }
-    })
-    (window as any).workbox.register()
+    const wb = new Workbox(sw, {scope: scope})
+    wb.register()
   }
 }
