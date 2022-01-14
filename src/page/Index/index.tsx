631b763cc4ed524ffa38e0e8c4b323c8f2956d2b
dxu <dean.xu@asml.com>
2022-01-14 22:18:55 +0800

    upgrade tailwind

--- src/page/Index/index.tsx
+++ src/page/Index/index.tsx
@@ -4,8 +4,8 @@ import css from './index.module.css';
 
 export const Index = () => {
   return (
-    <div className={''}>
-      <div className={'h-screen flex flex-col items-center justify-center space-y-4'}>
+    <div className={'h-screen overflow-y-scroll snap-y snap-mandatory scroll-smooth'}>
+      <div className={'h-screen flex flex-col items-center justify-center space-y-4 snap-center'}>
         <Image src={avatar} width={180} className={'rounded-full shadow overflow-hidden'}/>
         <div className={'text-6xl'}>XDean</div>
         <div className={css.links}>
@@ -15,7 +15,7 @@ export const Index = () => {
           <a href={'#about'}>关于</a>
         </div>
       </div>
-      <div className={'h-screen bg-black'}>
+      <div className={'h-screen bg-blue-300 snap-center'}>
 
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,9 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
-
-const myColor = {
-  bg: colors.white,
-  text: colors.black,
-  0: {0: colors.white, ...colors.coolGray},
-  1: {0: colors.white, ...colors.blue},
-  2: {0: colors.white, ...colors.green},
-}
-
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx,md,mdx}', './src/components/**/*.{js,ts,jsx,tsx,md,mdx}'],
-  darkMode: false,
-  theme: {
-    extend: {
-      colors: myColor,
-      textColor: myColor,
-      minWidth: {
-        ...defaultTheme.spacing,
-      },
-      maxWidth: {
-        ...defaultTheme.spacing,
-      }
-    },
-    flexGrow: {
-      DEFAULT: 1,
-      '0': 0,
-      '1': 1,
-      '2': 2,
-    },
-  },
-  variants: {
-    extend: {
-      zIndex: ['hover', 'active'],
-      scale: ['active'],
-      ringColor: ['hover'],
-      ringWidth: ['hover'],
-    },
-  },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './common/**/*.{js,ts,jsx,tsx,md,mdx}',
+  ],
+  theme: {},
   plugins: [],
-}
+};
