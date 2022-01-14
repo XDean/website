1b68148b225870f748a04bbf659657ba160ac65c
dxu <dean.xu@asml.com>
2022-01-14 08:51:59 +0800

    remove useless code

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const FooterView = () => {
-  return (
-    <div className={'text-sm'}>
-      Copyright © 2020-{new Date().getFullYear()} Dean Xu. All Rights reserved.
-    </div>
-  )
-}
--- src/util/array.ts
+++
@@ -1,3 +0,0 @@
-export function range(start, end) {
-  return [...Array(end - start).keys()].map(v => start + v)
-}
