ec652dc5ca15518019e4e746f93c3554c42d34d4
dxu <dean.xu@asml.com>
2022-01-18 18:01:41 +0800

    add quotes

---
+++ src/page/Index/data.ts
@@ -0,0 +1,92 @@
+export type Quote = {
+  en: string
+  zh: string
+  by: string
+}
+export const CoderQuotes: Quote[] = [
+  {
+    en: 'UNIX is simple. But It just needs a genius to understand its simplicity.',
+    zh: 'UNIX很简单(只针对那些有天赋的人)。',
+    by: 'Dennis Ritchie',
+  },
+  {
+    en: 'Before software can be reusable, it first has to be usable. ',
+    zh: '软件在能够复用前，它得能用。',
+    by: 'Ralph Johnson',
+  },
+  {
+    en: 'Good judgment comes from experience, and experience comes from bad judgment.',
+    zh: '优秀的判断力来自经验，而经验来自于错误的判断。',
+    by: 'Fred Brooks',
+  },
+  {
+    en: `The First Rule of Program Optimization: Don't do it. The Second Rule of Program Optimization (for experts only!): Don't do it yet.`,
+    zh: '程序优化第一原则：不要优化。程序优化第二原则(专家限定)：不要优化。',
+    by: 'Michael A. Jackson',
+  },
+  {
+    en: 'Get your data structures correct first, and the rest of the program will write itself.',
+    zh: '把数据理清，程序剩下的部分程序自会解决。',
+    by: 'David Jones',
+  },
+  {
+    en: `If we can't fix it, it ain't broke.`,
+    zh: '我们不是修不好，是你用得不对。',
+    by: 'Lieutenant Colonel Walt Weir',
+  },
+  {
+    en: `The fastest IO is no IO.`,
+    zh: '不要IO是最快的IO。',
+    by: 'Nils-Peter Nelson',
+  },
+  {
+    en: `Whenever possible, steal code.`,
+    zh: '君子善假于物也',
+    by: 'Tom Duff',
+  },
+  {
+    en: `Furious activity is no substitute for understanding.`,
+    zh: '磨刀不误砍柴工',
+    by: 'H. H. Williams',
+  },
+  {
+    en: `In order to understand recursion, you must first understand recursion`,
+    zh: '理解递归的第一步是：理解递归。',
+    by: 'Dean Joke',
+  },
+  {
+    en: `There are only two kinds of programming languages out there. The ones people complain about and the ones no one uses.`,
+    zh: '世界上只有两类编程语言：被人骂的，和没人用的',
+    by: 'Dean Joke',
+  },
+  {
+    en: `Stay hungry. Stay foolish.`,
+    zh: '求知若饥，虚心若愚。',
+    by: 'Steve Jobs',
+  },
+  {
+    en: `The purpose of software engineering is to control complexity, not to create it.`,
+    zh: '软件工程的目的是控制复杂性，而不是创建复杂性。',
+    by: 'Jon L. Bentley',
+  },
+  {
+    en: `The trouble with the world is that the stupid are cocksure and the intelligent full of doubt.`,
+    zh: '这个世界的一个问题是，愚者信誓旦旦，智者保持怀疑。',
+    by: 'Bertrand Russell',
+  },
+  {
+    en: ``,
+    zh: '',
+    by: '',
+  },
+  {
+    en: ``,
+    zh: '',
+    by: '',
+  },
+  {
+    en: ``,
+    zh: '',
+    by: '',
+  },
+];
