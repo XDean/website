7eaca17218cab7c6e92a43e78371c6d3988335bd
dxu <dean.xu@asml.com>
2022-01-19 08:49:37 +0800

    add quotes

--- src/page/Index/data.ts
+++ src/page/Index/data.ts
@@ -26,7 +26,7 @@ export const CodeQuotes: Quote[] = [
   },
   {
     en: 'Get your data structures correct first, and the rest of the program will write itself.',
-    zh: '把数据理清，程序剩下的部分程序自会解决。',
+    zh: '把数据厘清，程序自会解决剩下的部分。',
     by: 'David Jones',
   },
   {
@@ -57,7 +57,7 @@ export const CodeQuotes: Quote[] = [
   {
     en: `There are only two kinds of programming languages out there. The ones people complain about and the ones no one uses.`,
     zh: '世界上只有两类编程语言：被人骂的，和没人用的。',
-    by: 'Dean Joke',
+    by: 'Bjarne Stroustrup',
   },
   {
     en: `Stay hungry. Stay foolish.`,
@@ -71,7 +71,82 @@ export const CodeQuotes: Quote[] = [
   },
   {
     en: `The trouble with the world is that the stupid are cocksure and the intelligent full of doubt.`,
-    zh: '这个世界的一个问题是，愚者信誓旦旦，智者保持怀疑。',
+    zh: '愚者信誓旦旦，智者保持怀疑。',
     by: 'Bertrand Russell',
+  },
+  {
+    en: `Bad programmers worry about the code. Good programmers worry about data structures and their relationships.`,
+    zh: '低水平的程序员总在考虑代码，而高水平的关心数据的结构和关系。',
+    by: 'Linus Torvalds',
+  },
+  {
+    en: `The most damaging phrase in the language is.. it's always been done this way.`,
+    zh: '最具杀伤力短句：它以前一直这样。',
+    by: 'Grace Hopper',
+  },
+  {
+    en: `Testing leads to failure, and failure leads to understanding.`,
+    zh: '测试就会失败，失败才能理解。',
+    by: 'Burt Rutan',
+  },
+  {
+    en: `Programming isn't about what you know; it's about what you can figure out.`,
+    zh: '编程不是关于你会什么，而是关于你能会什么。',
+    by: 'Chris Pine',
+  },
+  {
+    en: `If debugging is the process of removing bugs, then programming must be the process of putting them in. `,
+    zh: '调试是解决BUG的过程，而编程是创造BUG的过程。',
+    by: 'Edsger W. Dikstra',
+  },
+  {
+    en: `There are two ways to write error-free programs; only the third works.`,
+    zh: '有两种方法可以写出没有BUG的代码，其中只有第三种有效。',
+    by: 'Alan J. Perlis',
+  },
+  {
+    en: `Man is a reed, the weakest of nature, but he is a thinking reed.`,
+    zh: '人是会思考的芦苇。',
+    by: 'Blaise Pascal',
+  },
+  {
+    en: `Nine people can’t make a baby in a month.`,
+    zh: '人月神话。',
+    by: 'Fred Brooks',
+  },
+  {
+    en: `Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.`,
+    zh: '调试的难度是编码的两倍。所以如果你费劲心里写代码，那你就没法调试它。',
+    by: 'Brian W. Kernighan',
+  },
+  {
+    en: `Any fool can write code that a computer can understand. Good programmers write code that humans can understand.`,
+    zh: '任何人都可以做到让计算机理解他们的代码。而优秀的程序员让人类理解他们的代码。',
+    by: 'Martin Fowler',
+  },
+  {
+    en: `One of my most productive days was throwing away 1000 lines of code.`,
+    zh: '记高效工作的一天：删除了1000行代码。',
+    by: 'Kem Thompson',
+  },
+  {
+    en: `First, solve the problem. Then, write the code.`,
+    zh: '写代码的第一步是解决问题。',
+    by: 'John Johnson',
+  },
+  {
+    en: `Talk is cheap. Show me the code.`,
+    zh: '废话少说，亮码。',
+    by: 'Linus Torvalds',
+  },
+  {
+    en: `I’ve finally learned what ‘upward compatible’ means.  It means we get to keep all our old mistakes.`,
+    zh: '我终于明白了"向上兼容"的真正含义：保留所有过去的错误。',
+    by: 'Dennie van Tassel',
+  },
+  {
+    en: `The first 90% of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.`,
+    zh: '前90%的代码会花费90%的时间，剩下的10%的代码花费另外90%的时间。',
+    by: '',
   },
 ];
