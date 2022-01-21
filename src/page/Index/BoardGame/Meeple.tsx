972e4eb098e90d495891dc4b55f1bf13ecc9da78
dxu <dean.xu@asml.com>
2022-01-21 14:07:41 +0800

    meeple animate

--- src/page/Index/BoardGame/Meeple.tsx
+++ src/page/Index/BoardGame/Meeple.tsx
@@ -1,3 +1,31 @@
-export const Meeple = () => {
+import clsx from 'clsx';
+import { useState } from 'react';
+import { GiMeeple } from 'react-icons/gi';
+import { random } from '../../../../common/util/array';
+import css from './index.module.css';
 
+const anims = [
+  '',
+  css.anim1,
+  css.anim2,
+  css.anim3,
+  css.anim4,
+  css.anim5,
+];
+
+type Props = {
+  className?: string
+}
+
+export const Meeple = (props: Props) => {
+  const {className} = props;
+  const [anim, setAnim] = useState(anims[0]);
+  return (
+    <div>
+      <GiMeeple
+        className={clsx(css.meeple, className, anim)}
+        onClick={() => setAnim(a => random(anims, [a]))}
+      />
+    </div>
+  );
 };
--- src/page/Index/BoardGame/index.tsx
+++ src/page/Index/BoardGame/index.tsx
@@ -1,58 +1,15 @@
-import {GiMeeple} from 'react-icons/gi';
-import css from './index.module.css';
 import clsx from 'clsx';
-import {useList} from 'react-use';
-
-const animationClasses = [
-  'animate-spin',
-  'animate-pulse',
-  'animate-bounce',
-  'rotate-180',
-  'scale-125',
-  'scale-75',
-];
+import css from './index.module.css';
+import { Meeple } from './Meeple';
 
 export const BoardGame = () => {
-  const classes = [];
-  for (let i = 0; i < 3; i++) {
-    const [leftClasses, setLeftClasses] = useList(() => [...animationClasses]);
-    const [meepleClass, setMeepleClass] = useList([]);
-    classes.push({
-      leftClasses,
-      setLeftClasses,
-      meepleClass,
-      setMeepleClass,
-    });
-  }
   return (
     <div className={'flex flex-col items-center font-mono'}>
-      <button className={'m-8'} onClick={() => {
-        const {
-          leftClasses,
-          setLeftClasses,
-          meepleClass,
-          setMeepleClass,
-        } = classes[Math.floor(Math.random() * 3)];
-        if ((Math.random() > 0.5 || meepleClass.length === 0) && leftClasses.length !== 0) {
-          const idx = Math.floor(leftClasses.length * Math.random());
-          const add = leftClasses[idx];
-          setLeftClasses.removeAt(idx);
-          setMeepleClass.push(add);
-        } else {
-          const idx = Math.floor(meepleClass.length * Math.random());
-          const add = meepleClass[idx];
-          setMeepleClass.removeAt(idx);
-          setLeftClasses.push(add);
-        }
-      }}>
-        add
-      </button>
       <div className={clsx(css.meepleContainer)}>
-        <GiMeeple className={clsx(css.meeple, css.m1, ...classes[0].meepleClass)}/>
-        <GiMeeple className={clsx(css.meeple, css.m2, ...classes[1].meepleClass)}/>
-        <GiMeeple className={clsx(css.meeple, css.m3, ...classes[2].meepleClass)}/>
+        <Meeple className={'text-red-500'}/>
+        <Meeple className={'text-blue-500'}/>
+        <Meeple className={'text-yellow-500'}/>
       </div>
-
     </div>
   );
 };
