7174f28733460ad6310da7cfcbbdca7f23fb346c
Dean Xu <373216024@qq.com>
2022-01-23 17:12:08 +0800

    add hello world alert

--- src/page/Index/Coding/index.tsx
+++ src/page/Index/Coding/index.tsx
@@ -1,15 +1,15 @@
-import { useMemo } from 'react';
-import { VscCode } from 'react-icons/vsc';
-import { CodeQuotes } from './data';
+import {useMemo} from 'react';
+import {VscCode} from 'react-icons/vsc';
+import {CodeQuotes} from './data';
 import css from './index.module.css';
-import { Quotes } from './Quotes';
-import { Wheel } from './Wheel';
+import {Quotes} from './Quotes';
+import {Wheel} from './Wheel';
 
 export const Coding = () => {
   return (
     <div className={'flex flex-col items-center font-mono'}>
       <VscCode size={180} className={'rounded-full shadow overflow-hidden'}/>
-      <div className={css.helloWorld}>Hello World!</div>
+      <div className={css.helloWorld} onClick={() => alert('Hello There!')}>Hello World!</div>
       <div className={'flex flex-row items-center space-x-2 text-xl'}>
         <div>I like</div>
         <Wheel shuffle words={useMemo(() => [
