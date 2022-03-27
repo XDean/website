793c77ef6e9eb4e702235735e6be8c0d7fe838aa
Dean Xu <373216024@qq.com>
2022-03-27 21:02:54 +0800

    add baidu analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
-import { AppProps } from 'next/dist/pages/_app';
+import {AppProps} from 'next/dist/pages/_app';
 import '../assets/styles/globals.css';
+import Head from 'next/head';
+import {BaiduAnalytics} from '../common/util/analytics/baidu';
 
 function MyApp({Component, pageProps, router}: AppProps) {
-  // useGA(router);
   return (
-    <Component {...pageProps}/>
+    <>
+      <Head>
+        <title>XDean的主页</title>
+        <BaiduAnalytics id={'0ed976ca2cbb9131a67d1b3b1da74aa2'}/>
+      </Head>
+      <Component {...pageProps}/>
+    </>
   );
 }
 
