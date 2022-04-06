b52b8141ba500554d568fdbb0f84083385c4fb02
dxu <dean.xu@asml.com>
2022-04-06 11:48:27 +0800

    update guobiao title

--- pages/tools/guobiao.tsx
+++ pages/tools/guobiao.tsx
@@ -1,7 +1,11 @@
-import {Redirect} from '../../common/components/Redirect';
+import Head from 'next/head';
+import { Redirect } from '../../common/components/Redirect';
 
 const Page = () => (
   <div className={'h-[90vh] flex flex-col justify-center'}>
+    <Head>
+      <title>国标麻将算番器 正在跳转</title>
+    </Head>
     <Redirect url={'https://tool.xdean.cn/tool/guobiao'}
               delay={3000}
               text={'国标算番器已经迁移到了新的域名'}
